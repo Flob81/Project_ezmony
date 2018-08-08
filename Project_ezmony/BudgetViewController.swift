145ed7041309ae0dc560be9b027a9679e3938cff
mawi10 <marcus.witschel@gmx.de>
2018-08-08 23:21:40 +0200

    +/- buttons & design

--- Project_ezmony/BudgetViewController.swift
+++ Project_ezmony/BudgetViewController.swift
@@ -11,9 +11,12 @@ import Charts
 
 class BudgetViewController: UIViewController {
 
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        configureButtonsDesign()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +24,20 @@ class BudgetViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    fileprivate func configureButtonsDesign() {
+        plusButton.backgroundColor = UIColorFromHex(rgbValue: 0x5DCE6B, alpha: 0.6)
+        plusButton.layer.cornerRadius = 25
+        minusButton.backgroundColor = UIColorFromHex(rgbValue: 0xE0837F, alpha: 0.6)
+        minusButton.layer.cornerRadius = 25
+    }
+    
+    func UIColorFromHex(rgbValue:UInt32, alpha:Double=1.0)->UIColor {
+        let red = CGFloat((rgbValue & 0xFF0000) >> 16)/256.0
+        let green = CGFloat((rgbValue & 0xFF00) >> 8)/256.0
+        let blue = CGFloat(rgbValue & 0xFF)/256.0
+        
+        return UIColor(red:red, green:green, blue:blue, alpha:CGFloat(alpha))
+    }
+    
 }
 
