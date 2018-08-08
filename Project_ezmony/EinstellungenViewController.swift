16a9914d1498f7a9e84d6eab4f7e2eb6a93363d7
Flob81 <florian.boersch@googlemail.com>
2018-08-08 22:30:03 +0200

    TabBar Controller & 4 ViewController eingebunden

---
+++ Project_ezmony/EinstellungenViewController.swift
@@ -0,0 +1,26 @@
+//
+//  EinstellungenViewController.swift
+//  Project_ezmony
+//
+//  Created by Florian Börsch on 08.08.18.
+//  Copyright © 2018 Bad Pixel. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Charts
+
+class EinstellungenViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
